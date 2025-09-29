9cb22dcc79497ebe74fb48627fb2cc3baad08457
Pranav2325 <pranavdhondkar@gmail.com>
2025-09-29 21:01:14 +0530

    create cardetailscard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import { dummyCarData } from '../assets/assets'
+import CarCard from '../components/CarCard'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      {dummyCarData.map((car)=>(
+        <CarCard key={car} car={car}/>
+      ))}
     </>
   )
 }
