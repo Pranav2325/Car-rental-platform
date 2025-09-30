b4cfa97b1e59ff8c5b9518a216abefcea066a69c
Pranav2325 <pranavdhondkar@gmail.com>
2025-09-30 20:47:46 +0530

    create list your car banner

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from '../components/Hero'
 import { dummyCarData } from '../assets/assets'
 import CarCard from '../components/CarCard'
 import FeaturedDection from '../components/FeaturedDection'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDection/>
+      <Banner/>
       
     </>
   )
